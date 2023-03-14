fa4c105b734ac1eedbbd2215a0d4789e1f53735b
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2023-03-14 18:19:29 +0530

    added: count how many numbers are divisible by K
    refer: https://www.codechef.com/LP0TO101/problems/INTEST

---
+++ Hardcore-Algorithms/basics/count_divisibles.py
@@ -0,0 +1,29 @@
+"""
+    Given an input of N integers find the numbers which are divisible by K
+
+    TC:
+        7 3
+        1
+        51
+        966369
+        7
+        9
+        999996
+        11 
+"""
+# First line of input is the values of n, k
+
+# consume input
+n, k = input().split()
+n = int(n)
+k = int(k)
+
+count = 0
+for i in range(n):
+    number = int(input())
+    if number % k == 0:
+        count = count + 1
+
+print(count)
+    
+
