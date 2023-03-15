dc4abf932cf978c77bfac257c4ae7933aadec128
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2023-03-15 16:41:59 +0530

    refactor: the loop to std lib functions that do the same thing.

--- Hardcore-Algorithms/basics/find_sum_of_digits.py
+++ Hardcore-Algorithms/basics/find_sum_of_digits.py
@@ -4,7 +4,6 @@
 total_cases = int(input())
 
 for test_case in range(total_cases):
-    total_sum = 0
-    for digit in input():
-        total_sum = total_sum + int(digit)
+    digits = map(int, input())
+    total_sum = sum(digits)
     print(total_sum)
