314055a9028881b2f748227f3656afeafe2b0d09
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2023-04-09 04:36:47 +0530

    added: react-native app with basic navigation.

---
+++ Hardcore-Frameworks/react-native/ExpoRouter/app/profile.js
@@ -0,0 +1,39 @@
+import  { Text, SafeAreaView, StyleSheet, Button} from 'react-native';
+import { useRouter , useSearchParams} from 'expo-router';
+
+const profile = () => {
+    const router = useRouter();
+    const {name} = useSearchParams();
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.subtitle}>This is profile view for {name}</Text>
+            <Button onPress={() => router.back()} title={"Go back!"}/>
+        </SafeAreaView>
+    )
+}
+
+export default profile;
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      padding: 24,
+    },
+    main: {
+      flex: 1,
+      justifyContent: "center",
+      maxWidth: 960,
+      marginHorizontal: "auto",
+    },
+    title: {
+      fontSize: 64,
+      fontWeight: "bold",
+    },
+    subtitle: {
+      fontSize: 36,
+      color: "#38434D",
+    },
+  });
