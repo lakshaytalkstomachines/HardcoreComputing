22bd24c75206a4f53383d639650550fe64c5602c
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2024-04-22 03:01:11 +0530

    ability to create nodes without data.

--- Hardcore-Data-Structures/tree.py
+++ Hardcore-Data-Structures/tree.py
@@ -2,20 +2,22 @@
 Basic Implementation of a tree.
 """
 
+from typing import Optional
+
 
 class Node:
     """
     This defines a unit of data (integer).
     """
 
-    def __init__(self, data: int) -> None:
+    def __init__(self, data: Optional[int] = None) -> None:
         """
         Initializes data.
         """
         self.data = data
 
     def __str__(self) -> str:
-        return f"Node({self.data})"
+        return f"Node({'--' if not self.data else self.data})"
 
 
 def main():
@@ -28,6 +30,9 @@ def main():
 
     for node in nodeList:
         print(node)
+
+    # Special Node with no Data.
+    print(Node())
 
 
 if __name__ == "__main__":
