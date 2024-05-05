2674c3c9b4c685f51408963c5afbbdd2d5fe7611
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2024-05-06 01:11:20 +0530

    init simple app that send hello world.

--- Hardcore-Frameworks/telemetery/app.js
+++ Hardcore-Frameworks/telemetery/app.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || "8080";
 const express = require("express");
 const app = express();
 
-app.get("/", (res, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
